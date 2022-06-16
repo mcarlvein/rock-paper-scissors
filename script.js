5cefec5ff39826ed779a0f3c8d1f72b6e4f3f415
mcarlvein <michaelcarlvein@gmail.com>
2022-06-16 18:23:04 +1000

    Added html interface

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ function computerPlay() {
     return moves[choice];
 }
 
+let playerTotal = 0;
+let computerTotal = 0;
+
 function playRound(playerChoice, computerChoice) {
 
     if (playerChoice === computerChoice) {
@@ -15,28 +18,49 @@ function playRound(playerChoice, computerChoice) {
         (playerChoice === 'rock' && computerChoice === 'scissors')
 
     ) {
+        playerTotal++;
         return `${playerChoice} beats ${computerChoice}. You win!`;
     } else {
+        computerTotal++;
         return `${computerChoice} beats ${playerChoice}. You lose!`;
     }
 
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
+// function game() {
+//     for (let i = 0; i < 5; i++) {
 
-        let playerChoice = window.prompt('Scissors, Paper or Rock?');
-        playerChoice = playerChoice.toLowerCase();
+//         let playerChoice = window.prompt('Scissors, Paper or Rock?');
+//         playerChoice = playerChoice.toLowerCase();
 
-        const possibleMoves = ['scissors', 'paper', 'rock'];
+//         const possibleMoves = ['scissors', 'paper', 'rock'];
 
-        while (!possibleMoves.includes(playerChoice)) {
-            playerChoice = window.prompt('Invalid Choice. Please pick from Scissors, Paper or Rock!');
+//         while (!possibleMoves.includes(playerChoice)) {
+//             playerChoice = window.prompt('Invalid Choice. Please pick from Scissors, Paper or Rock!');
+//         }
+
+//         const computerChoice = computerPlay();
+//         console.log(playRound(playerChoice, computerChoice));
+
+//     }
+// }
+
+const buttons = document.querySelectorAll('button');
+const outcome = document.querySelector('#outcome');
+const playerScore = document.querySelector('#playerScore');
+const computerScore = document.querySelector('#computerScore');
+const finalOutcome = document.querySelector('#finalOutcome');
+buttons.forEach(button => 
+    button.addEventListener('click', (e) => {
+        const playerChoice = e.target.getAttribute('rps-option');
+        outcome.textContent = playRound(playerChoice, computerPlay());
+        playerScore.textContent = playerTotal;
+        computerScore.textContent = computerTotal;
+        if (playerTotal >= 5) {
+            finalOutcome.textContent = 'You made it to 5 victories. You Win!';
+        } else if (computerTotal >= 5) {
+            finalOutcome.textContent = 'Computer made it to 5 victories. You Lose!';
         }
-
-        const computerChoice = computerPlay();
-        console.log(playRound(playerChoice, computerChoice));
-
-    }
-}
+    })
+);
 
